1a67b2cbda28a9749cc98ce239eb013952fab900
Pipa505 <miguel.camacho58@outlook.com>
2022-06-04 14:13:06 -0500

    Init

---
+++ src/test/java/com/camacho/pract/PractApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camacho.pract;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PractApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
